34fdd8951fc68696f8c09be3384159b58961da20
Zakhar Menshikov <zm681986@mail.ru>
2023-05-29 23:09:03 +0500

    menu

--- scripts/index.js
+++ scripts/index.js
@@ -88,8 +88,15 @@ $(function () {
 
 
 //функция открытия меню
-const handleClick = function () {
+const openClick = function () {
     const divs = document.getElementById("menu")
-    divs.classList.toggle("hide")
+    divs.classList.remove("hide")
 }
-document.getElementById("menu-toggle").addEventListener("click", handleClick)
+document.getElementById("menu-toggle").addEventListener("click", openClick)
+
+//функция закрытия меню
+const closeClick = function () {
+    const divs = document.getElementById("menu")
+    divs.classList.add("hide")
+}
+document.getElementById("menu-close").addEventListener("click", closeClick)
